f8d973b5ec75b5ee560990643cf595f7ffc3449c
Maksim <Daymansolvatore88@gmail.com>
2025-10-08 12:55:25 +0400

    feat(router): background location modals + page routes for ingredients and orders

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,7 +3,7 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation, type Location } from 'react-router-dom';
 import { IngredientDetails, OrderInfo, Modal } from '@components';
 import { useCallback, useEffect, ReactElement } from 'react';
 import { useDispatch, useSelector } from '../../services/store';
@@ -24,6 +24,8 @@ const GuestRoute = ({ element, isAuth, isAuthChecked }: { element: ReactElement;
 const App = () => {
   const { user, isAuthChecked } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
+  const location = useLocation();
+  const state = location.state as { background?: Location } | undefined;
 
   useEffect(() => {
     if (!isAuthChecked) {
@@ -38,7 +40,8 @@ const App = () => {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Routes>
+      {/* Базовые страничные маршруты */}
+      <Routes location={state?.background || location}>
         <Route path="/" element={<ConstructorPage />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/login" element={<GuestRoute isAuthChecked={isAuthChecked} isAuth={Boolean(user)} element={<Login />} />} />
@@ -47,39 +50,48 @@ const App = () => {
         <Route path="/reset-password" element={<GuestRoute isAuthChecked={isAuthChecked} isAuth={Boolean(user)} element={<ResetPassword />} />} />
         <Route path="/profile" element={<ProtectedRoute isAuthChecked={isAuthChecked} isAuth={Boolean(user)} element={<Profile />} />} />
         <Route path="/profile/orders" element={<ProtectedRoute isAuthChecked={isAuthChecked} isAuth={Boolean(user)} element={<ProfileOrders />} />} />
+        {/* Страничные версии деталей */}
+        <Route path="/ingredients/:id" element={<IngredientDetails />} />
+        <Route path="/feed/:number" element={<OrderInfo />} />
+        <Route path="/profile/orders/:number" element={<ProtectedRoute isAuthChecked={isAuthChecked} isAuth={Boolean(user)} element={<OrderInfo />} />} />
         <Route path="*" element={<NotFound404 />} />
-
-        <Route
-          path="/feed/:number"
-          element={
-            <Modal title="" onClose={handleCloseModal}>
-              <OrderInfo />
-            </Modal>
-          }
-        />
-        <Route
-          path="/ingredients/:id"
-          element={
-            <Modal title="Детали ингредиента" onClose={handleCloseModal}>
-              <IngredientDetails />
-            </Modal>
-          }
-        />
-        <Route
-          path="/profile/orders/:number"
-          element={
-            <ProtectedRoute
-              isAuthChecked={isAuthChecked}
-              isAuth={Boolean(user)}
-              element={
-                <Modal title="" onClose={handleCloseModal}>
-                  <OrderInfo />
-                </Modal>
-              }
-            />
-          }
-        />
       </Routes>
+
+      {/* Модальные маршруты поверх background */}
+      {state?.background && (
+        <Routes>
+          <Route
+            path="/feed/:number"
+            element={
+              <Modal title="" onClose={handleCloseModal}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path="/ingredients/:id"
+            element={
+              <Modal title="Детали ингредиента" onClose={handleCloseModal}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path="/profile/orders/:number"
+            element={
+              <ProtectedRoute
+                isAuthChecked={isAuthChecked}
+                isAuth={Boolean(user)}
+                element={
+                  <Modal title="" onClose={handleCloseModal}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+            }
+          />
+        </Routes>
+      )}
     </div>
   );
 };
